0881c52bb0d4008a76edda1b303557e1ad65d67f
Oliver Binns <mail@oliverbinns.co.uk>
2024-03-29 10:07:03 +0000

    test: added a new test for CaptureViewModel

--- CaptureTests/Photos/Capture/CaptureViewModelTests.swift
+++ CaptureTests/Photos/Capture/CaptureViewModelTests.swift
@@ -1,7 +1,25 @@
+@testable import Capture
 import XCTest
 
 final class CaptureViewModelTests: XCTestCase {
-    func testImplementTests() {
-        XCTFail("todo: implement tests")
+    var sut: CaptureViewModel!
+    
+    @MainActor
+    override func setUp() {
+        super.setUp()
+        
+        sut = CaptureViewModel(
+            cameraPermissionManager: MockCameraPermissionManager.self
+        )
+    }
+    
+    @MainActor
+    func testCorrectlyFetchesPermissions() {
+        XCTAssertEqual(MockCameraPermissionManager.didCheckAuthorizationStatus, .video)
+        XCTAssertEqual(sut.authorizationStatus, .notDetermined)
+        
+        MockCameraPermissionManager.authorizationStatus = .authorized
+        sut = CaptureViewModel(cameraPermissionManager: MockCameraPermissionManager.self)
+        XCTAssertEqual(sut.authorizationStatus, .authorized)
     }
 }
