dca3b56809c8b6cf8f9d72e2693c4ce4b5d7f0aa
Oliver Binns <mail@oliverbinns.co.uk>
2024-07-27 17:55:34 +0100

    feat: no-content message

--- Capture/Film/FilmList.swift
+++ Capture/Film/FilmList.swift
@@ -4,15 +4,25 @@ import SwiftUI
 struct FilmList: View {
     @Query var rolls: [FilmRoll]
     @Environment(\.modelContext) private var modelContext
-    
+
+
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(rolls) { roll in
-                    FilmView(film: roll)
-                        .frame(maxWidth: .infinity)
-                        .listRowSeparator(.hidden)
-                        .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+            Group {
+                if rolls.count == 0 {
+                    List {
+                        ContentUnavailableView("Add a film roll to get started!",
+                                               systemImage: "film.stack.fill")
+                    }
+                } else {
+                    List {
+                        ForEach(rolls) { roll in
+                            FilmView(film: roll)
+                                .frame(maxWidth: .infinity)
+                                .listRowSeparator(.hidden)
+                                .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+                        }
+                    }.listStyle(.plain)
                 }
             }
             .navigationTitle("Photos")
@@ -25,7 +35,6 @@ struct FilmList: View {
                 }
             }
         }
-        .listStyle(.plain)
         .background(.secondary)
         .tabItem {
             Label("Photos", systemImage: "film.stack.fill")
